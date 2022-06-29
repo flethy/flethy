922bc296276b8248b95396621e24e4e846b9b7b4
Adam <adam.urban@gmail.com>
2022-06-29 16:05:59 +0200

    feat(flow): use jq

--- packages/flow/src/test.ts
+++ packages/flow/src/test.ts
@@ -2,11 +2,14 @@ import { nao, OpenSea, Slack } from '@web3nao/http-configs'
 import { FetchParams } from '@web3nao/http-configs/dist/types/FetchParams.type'
 import { RequestParams } from '@web3nao/http-configs/dist/types/Request.types'
 import axios from 'axios'
+import * as jq from 'node-jq'
 
 interface naoEntry extends RequestParams {
   id: string
   [key: string]: any
 }
+
+const JQ_TYPE_SEPARATOR = '->'
 
 const FLOW: naoEntry[] = [
   {
@@ -15,7 +18,7 @@ const FLOW: naoEntry[] = [
     'auth:X-API-KEY': process.env.OPENSEA_APIKEY!,
     'query:asset_contract_address': process.env.ETH_DIYPUNKS_CONTRACT!,
     'query:owner': process.env.ETH_OWNER!,
-    'query:limit': 'context.limit|number',
+    'query:limit': '->.context.limit->number',
     'query:offset': 0,
     'query:order_direction': 'desc',
   },
@@ -29,7 +32,7 @@ const FLOW: naoEntry[] = [
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: `context.assets[0].id|string`,
+          text: `->.context.assets[0].id->string`,
         },
       },
     ],
@@ -40,56 +43,13 @@ export class Test {
   public async flowGeneric(flow: naoEntry[], input: any) {
     let context = input
     for (const entry of flow) {
-      Test.changeVars(entry, context)
+      await Test.changeVars(entry, context)
       const request = nao(entry)
       console.log({ request })
       const response = await Test.request(request)
       context = Object.assign(context, response)
       console.log({ context })
     }
-  }
-
-  public async flow(input: { limit: number }) {
-    let response: any
-
-    console.log(`opensea`)
-
-    response = await Test.request(
-      nao<OpenSea.GetAssets>({
-        kind: 'opensea.assets.get',
-        'auth:X-API-KEY': process.env.OPENSEA_APIKEY!,
-        'query:asset_contract_address': process.env.ETH_DIYPUNKS_CONTRACT!,
-        'query:owner': process.env.ETH_OWNER!,
-        'query:limit': input.limit,
-        'query:offset': 0,
-        'query:order_direction': 'desc',
-      }),
-    )
-
-    console.log(`slack`)
-
-    try {
-      response = await Test.request(
-        nao({
-          kind: 'slack.incomingWebhooks.message',
-          'auth:webhookid': process.env.SLACK_WEBHOOK_ID!,
-          'body:text': 'Hello, world!',
-          'body:blocks': [
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: `num sales: ${response.assets[0].id}`,
-              },
-            },
-          ],
-        }),
-      )
-    } catch (error) {
-      console.log(error)
-    }
-
-    // console.log(response)
   }
 
   public static async request(params: FetchParams) {
@@ -106,28 +66,35 @@ export class Test {
     return data
   }
 
-  public static changeVars(object: any, context: any) {
+  public static async changeVars(object: any, context: any) {
     for (const key of Object.keys(object)) {
       if (typeof object[key] === 'string') {
         const stringValue: string = object[key]
-        if (stringValue.startsWith('context.')) {
-          const splitted = stringValue.split('|')
-          const evaluated = eval(splitted[0])
-          switch (splitted[1]) {
+        if (stringValue.startsWith(JQ_TYPE_SEPARATOR)) {
+          const splitted = stringValue.split(JQ_TYPE_SEPARATOR)
+          const evaluated = await jq.run(
+            splitted[1],
+            { context },
+            { input: 'json' },
+          )
+          switch (splitted[2]) {
             case 'number':
               object[key] = Number(evaluated)
               break
             case 'string':
               object[key] = String(evaluated)
               break
+            default:
+              object[key] = evaluated
           }
         }
       } else if (Array.isArray(object[key])) {
-        object[key].forEach((child: any) => {
-          Test.changeVars(child, context)
-        })
+        const promises = object[key].map((child: any) =>
+          Test.changeVars(child, context),
+        )
+        await Promise.all(promises)
       } else if (typeof object[key] === 'object') {
-        Test.changeVars(object[key], context)
+        await Test.changeVars(object[key], context)
       }
     }
   }
