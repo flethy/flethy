ce44ca66365a20e6547c3c53babaf5430ee90e08
Adam <adam.urban@gmail.com>
2022-09-18 06:37:52 +0200

    feat(landingpage): add functions

---
+++ apps/landing-page/functions/api/test.ts
@@ -0,0 +1,14 @@
+export const jsonResponse = (value: any, init: ResponseInit = {}) =>
+	new Response(JSON.stringify(value), {
+		headers: { 'Content-Type': 'application/json', ...init.headers },
+		...init,
+	})
+
+export const onRequestGet: PagesFunction<{
+	VITE_LOGLEVEL: string
+}> = async ({ request, env }) => {
+	return jsonResponse({
+		status: 'hf!',
+		setting: env.VITE_LOGLEVEL,
+	})
+}
