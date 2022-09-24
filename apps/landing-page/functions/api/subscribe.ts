0831fe625d531c10a81d7e8723ad02462219e13e
Adam <adam.urban@gmail.com>
2022-09-24 15:53:45 +0200

    chore(lp): error message

--- apps/landing-page/functions/api/subscribe.ts
+++ apps/landing-page/functions/api/subscribe.ts
@@ -17,71 +17,79 @@ export const onRequestPost: PagesFunction<{
 			{ status: 400 },
 		)
 	}
-	const email = body.email.toLowerCase()
 
-	const isPendingAvailable = await KvUtils.isPendingEntryAvailable({ email })
-	if (isPendingAvailable) {
+	try {
+		const email = body.email.toLowerCase()
+
+		const isPendingAvailable = await KvUtils.isPendingEntryAvailable({ email })
+		if (isPendingAvailable) {
+			return jsonResponse({
+				status: 'done',
+			})
+		}
+
+		const now = new Date()
+
+		const pendingSubscription: PendingSubscription = {
+			email: body.email,
+			ts: now.getTime(),
+			date: now.toISOString(),
+			token: crypto.randomUUID(),
+		}
+
+		const flow: FlowNode[] = [
+			{
+				id: 'sendVerificationEmail',
+				config: {
+					namespace: 'mailjet',
+				},
+				kind: 'mailjet.send.basicEmail',
+				'auth:Authorization': {
+					username: '==>secrets==>MAILJET_API_KEY',
+					password: '==>secrets==>MAILJET_API_SECRET',
+				},
+				'body:Messages': [
+					{
+						From: {
+							Email: 'adam@flethy.com',
+							Name: 'Adam (from flethy)',
+						},
+						To: [
+							{
+								Name: pendingSubscription.email,
+								Email: pendingSubscription.email,
+							},
+						],
+						Subject: `Hello from flethy!`,
+						HTMLPart: `<h3>Hello from flethy!</h3><p>Please verify your email address: <a href="https://flethy.com/verify/${pendingSubscription.token}">https://flethy.com/verify/${pendingSubscription.token}</a></p>`,
+					},
+				],
+			},
+		]
+
+		const engine = new FlowEngine({
+			flow,
+			input: {},
+			env: {
+				env: {},
+				secrets: {
+					MAILJET_API_KEY: env.MAILJET_API_KEY,
+					MAILJET_API_SECRET: env.MAILJET_API_SECRET,
+				},
+			},
+		})
+
+		await engine.start()
+
+		await KvUtils.addPendingEntry(pendingSubscription)
+
 		return jsonResponse({
 			status: 'done',
 		})
+	} catch (error: any) {
+		return jsonResponse(
+			{ status: 'error', message: error.message, error },
+			{ status: 500 },
+		)
 	}
-
-	const now = new Date()
-
-	const pendingSubscription: PendingSubscription = {
-		email: body.email,
-		ts: now.getTime(),
-		date: now.toISOString(),
-		token: crypto.randomUUID(),
-	}
-
-	const flow: FlowNode[] = [
-		{
-			id: 'sendVerificationEmail',
-			config: {
-				namespace: 'mailjet',
-			},
-			kind: 'mailjet.send.basicEmail',
-			'auth:Authorization': {
-				username: '==>secrets==>MAILJET_API_KEY',
-				password: '==>secrets==>MAILJET_API_SECRET',
-			},
-			'body:Messages': [
-				{
-					From: {
-						Email: 'adam@flethy.com',
-						Name: 'Adam (from flethy)',
-					},
-					To: [
-						{
-							Name: pendingSubscription.email,
-							Email: pendingSubscription.email,
-						},
-					],
-					Subject: `Hello from flethy!`,
-					HTMLPart: `<h3>Hello from flethy!</h3><p>Please verify your email address: <a href="https://flethy.com/verify/${pendingSubscription.token}">https://flethy.com/verify/${pendingSubscription.token}</a></p>`,
-				},
-			],
-		},
-	]
-
-	const engine = new FlowEngine({
-		flow,
-		input: {},
-		env: {
-			env: {},
-			secrets: {
-				MAILJET_API_KEY: env.MAILJET_API_KEY,
-				MAILJET_API_SECRET: env.MAILJET_API_SECRET,
-			},
-		},
-	})
-
-	await engine.start()
-
-	await KvUtils.addPendingEntry(pendingSubscription)
-
-	return jsonResponse({
-		status: 'done',
-	})
 }
