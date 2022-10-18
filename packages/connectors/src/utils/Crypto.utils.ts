f6d700aea69ab955f85d3a6b36fd6d895f84a263
Adam <adam.urban@gmail.com>
2022-10-18 15:58:24 +0200

    chore(core): web crypto

--- packages/connectors/src/utils/Crypto.utils.ts
+++ packages/connectors/src/utils/Crypto.utils.ts
@@ -1,10 +1,53 @@
 export class CryptoUtils {
-  public static createHmacBase64(algorithm: string, key: string, base: string) {
+  public static async createHmacBase64(
+    algorithm: string,
+    key: string,
+    base: string,
+  ) {
     if (typeof crypto !== 'undefined') {
       // TODO: check web crypto implementation
       // https://developers.cloudflare.com/workers/runtime-apis/web-crypto/
       // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/sign
       // https://nodejs.org/api/webcrypto.html
+
+      var enc = new TextEncoder()
+
+      const encodedKey = await crypto.subtle.importKey(
+        'raw', // raw format of the key - should be Uint8Array
+        enc.encode(key),
+        {
+          // algorithm details
+          name: 'HMAC',
+          hash: { name: algorithm },
+        },
+        false, // export = false
+        ['sign', 'verify'], // what this key can do
+      )
+
+      crypto.subtle
+        .importKey(
+          'raw', // raw format of the key - should be Uint8Array
+          enc.encode(key),
+          {
+            // algorithm details
+            name: 'HMAC',
+            hash: { name: algorithm },
+          },
+          false, // export = false
+          ['sign', 'verify'], // what this key can do
+        )
+        .then((key) => {
+          window.crypto.subtle
+            .sign('HMAC', key, enc.encode(base))
+            .then((signature) => {
+              var b = new Uint8Array(signature)
+              var str = Array.prototype.map
+                .call(b, (x) => x.toString(16).padStart(2, '0'))
+                .join('')
+              console.log(str)
+            })
+        })
+
       throw new Error(`CryptoUtils | To be implemented with WebCrypto.`)
     } else {
       try {
