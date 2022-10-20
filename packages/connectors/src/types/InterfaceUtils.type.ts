bc613b78ec205c3a24dfb363c15a7d44ecce9ba5
Adam <adam.urban@gmail.com>
2022-10-20 07:12:56 +0200

    feat(core): interface conditions

---
+++ packages/connectors/src/types/InterfaceUtils.type.ts
@@ -0,0 +1,24 @@
+export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
+  }[Keys]
+
+export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> &
+      Partial<Record<Exclude<Keys, K>, undefined>>
+  }[Keys]
+
+// Example usage:
+// interface TestOne {
+//   test1?: string
+//   test2?: string
+// }
+
+// type TestOneRequired = RequireAtLeastOne<TestOne, 'test1' | 'test2'>
