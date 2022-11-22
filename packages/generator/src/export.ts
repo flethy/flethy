dac500a163c3e21e327ef25109cfa6e80e1485ed
Adam <adam.urban@gmail.com>
2022-11-22 17:28:58 +0100

    feat(gen): add use cases

--- packages/generator/src/export.ts
+++ packages/generator/src/export.ts
@@ -2,12 +2,14 @@ import { ConfigsExporter } from './exporters/configs.exporter'
 import { DocsExporter } from './exporters/docs.exporter'
 import { SitemapExporter } from './exporters/sitemap.exporter'
 import { TypesExporter } from './exporters/types.exporter'
+import { UseCasesExporter } from './exporters/usecases.exporter'
 
 async function main() {
   await ConfigsExporter.export()
   await DocsExporter.export()
   await TypesExporter.export()
   await SitemapExporter.export()
+  await UseCasesExporter.export()
 }
 
 main()
