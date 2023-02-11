8c1b43a376f91d2137beb7190b1605ae9dffa6f9
Adam <adam.urban@gmail.com>
2023-02-11 08:17:34 +0100

    feat(configs): dune

--- packages/connectors/src/wip/dune.config.ts
+++ packages/connectors/src/wip/dune.config.ts
@@ -5,11 +5,28 @@ import {
 import { RequestParams } from '../types/Request.types'
 
 export namespace Dune {
-  export type Entity = { test: any }
-  export type Endpoint = { test: ApiDescriptionEndpoint }
+  export type Entity = { query: any; execution: any }
+  export type Endpoint =
+    | { execute: ApiDescriptionEndpoint }
+    | { status: ApiDescriptionEndpoint; results: ApiDescriptionEndpoint }
 
-  export interface Test extends RequestParams {
-    kind: 'dune..'
+  interface DuneBase {
+    'auth:x-dune-api-key': string
+  }
+
+  export interface ExecuteQuery extends DuneBase, RequestParams {
+    kind: 'dune.query.execute'
+    'param:queryId': string
+  }
+
+  export interface ExecutionStatus extends DuneBase, RequestParams {
+    kind: 'dune.execution.status'
+    'param:executionId': string
+  }
+
+  export interface ExecutionResults extends DuneBase, RequestParams {
+    kind: 'dune.execution.results'
+    'param:executionId': string
   }
 
   export const API: ApiDescription<Entity, Endpoint> = {
@@ -27,20 +44,81 @@ export namespace Dune {
         twitter: 'DuneAnalytics',
       },
     },
-    base: '',
+    base: 'https://api.dune.com/api/v1',
+    auth: {
+      'x-dune-api-key': {
+        type: 'header',
+      },
+    },
     api: {
-      test: {
-        test: {
-          interface: '',
+      query: {
+        execute: {
+          interface: 'ExecuteQuery',
           meta: {
-            title: '',
-            description: '',
-            docs: '',
+            title: 'Execute Query ID',
+            description: 'Execute Query ID',
+            docs: 'https://dune.com/docs/api/api-reference/execute-query-id/',
+          },
+          method: 'POST',
+          paths: [
+            {
+              name: 'query',
+              type: 'static',
+            },
+            {
+              name: 'queryId',
+              type: 'param',
+            },
+            {
+              name: 'execute',
+              type: 'static',
+            },
+          ],
+        },
+      },
+      execution: {
+        status: {
+          interface: 'ExecutionStatus',
+          meta: {
+            title: 'Execution Status',
+            description: 'Execution Status',
+            docs: 'https://dune.com/docs/api/api-reference/execution-status/',
           },
           method: 'GET',
           paths: [
             {
-              name: '',
+              name: 'execution',
+              type: 'static',
+            },
+            {
+              name: 'executionId',
+              type: 'param',
+            },
+            {
+              name: 'status',
+              type: 'static',
+            },
+          ],
+        },
+        results: {
+          interface: 'ExecutionResults',
+          meta: {
+            title: 'Execution Results',
+            description: 'Execution Results',
+            docs: 'https://dune.com/docs/api/api-reference/execution-results/',
+          },
+          method: 'GET',
+          paths: [
+            {
+              name: 'execution',
+              type: 'static',
+            },
+            {
+              name: 'executionId',
+              type: 'param',
+            },
+            {
+              name: 'results',
               type: 'static',
             },
           ],
