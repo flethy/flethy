78ea4ac53b9585047520534ac1e5f637b392cb5f
Adam <adam.urban@gmail.com>
2023-03-05 08:23:35 +0100

    feat(templates): new structure

--- packages/templates/src/types/template.types.ts
+++ packages/templates/src/types/template.types.ts
@@ -1,5 +1,38 @@
 import { FlowNode } from '@flethy/flow'
 
+export type TemplateLevel = 'Beginner' | 'Intermediate' | 'Advanced'
+export type TemplateCategory = 'social-media'
+export interface Template {
+  id: string
+  level: TemplateLevel
+  meta: {
+    name: string
+    description: string
+    prereq: Array<{
+      text: string
+      link?: {
+        href: string
+        label: string
+      }
+      secrets?: string[]
+    }>
+    docs?: Array<{
+      type: 'docs' | 'blog'
+      href: string
+      label: string
+    }>
+    services: string[]
+    category: TemplateCategory
+    tags: string[]
+  }
+  flow: {
+    nodes: FlowNode[]
+    env?: {
+      [key: string]: string
+    }
+  }
+}
+
 export enum TutorialLevel {
   Beginner = 'Beginner',
   Intermediate = 'Intermediate',
